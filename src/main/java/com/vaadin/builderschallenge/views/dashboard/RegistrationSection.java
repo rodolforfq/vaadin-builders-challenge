2db27de8cb2c8171408b399c80a7e4e917966346
Joel Robertson <joel@vaadin.com>
2023-04-12 15:30:58 -0600

    Added charts dependency & stubbed in a few charts.

--- src/main/java/com/vaadin/builderschallenge/views/dashboard/RegistrationSection.java
+++ src/main/java/com/vaadin/builderschallenge/views/dashboard/RegistrationSection.java
@@ -2,7 +2,8 @@ package com.vaadin.builderschallenge.views.dashboard;
 
 import com.vaadin.builderschallenge.services.DashboardService;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.ChartType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,15 +17,24 @@ public class RegistrationSection extends Section {
         LOG.info("RegistrationMetrics: {}", registrationMetrics);
 
 
-        addTile("Registration", createRegistrationWidget());
-        addTile("Participation", createParticipationWidget());
+        addTile(createRegistrationWidget());
+        addTile(createParticipationWidget());
     }
 
-    private static Component createRegistrationWidget() {
-        return new Text("A dial showing number of employees registered vs. total in company");
+    private Component createRegistrationWidget() {
+        var chart = new Chart(ChartType.GAUGE);
+        chart.getConfiguration().setTitle("Registration");
+        chart.getConfiguration().setSubTitle("The number of employees registered vs. total in company");
+
+        return chart;
     }
 
-    private static Component createParticipationWidget() {
-        return new Text("A dial showing number of employee-days registered vs. number of employee-days possible (try to show party part of dial in a different color—or even each day's sum as a different color)");
+    // try to show party part of dial in a different color—or even each day's sum as a different color
+    private Component createParticipationWidget() {
+        var chart = new Chart(ChartType.GAUGE);
+        chart.getConfiguration().setTitle("Participation");
+        chart.getConfiguration().setSubTitle("The number of employee-days registered vs. number of employee-days possible");
+
+        return chart;
     }
 }
--- src/main/java/com/vaadin/builderschallenge/views/dashboard/Tile.java
+++ src/main/java/com/vaadin/builderschallenge/views/dashboard/Tile.java
@@ -2,6 +2,7 @@ package com.vaadin.builderschallenge.views.dashboard;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.theme.lumo.LumoUtility;
@@ -12,10 +13,16 @@ public class Tile extends Composite<VerticalLayout> {
         var labelSlot = new H2(label);
 
         var content = getContent();
+        content.setPadding(false);
         content.addClassNames(LumoUtility.Border.ALL,
                 LumoUtility.BorderRadius.LARGE,
                 LumoUtility.BorderColor.CONTRAST_10);
+        content.setHeight(50, Unit.EX);
         content.add(labelSlot);
         content.add(widget);
+    }
+
+    public Tile(Component widget) {
+        this(null, widget);
     }
 }
